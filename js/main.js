d3c84d8040ebd924b12622a95d4f461efa82efbd
christineywang <wang.christiney@gmail.com>
2019-01-12 12:08:51 -0800

    Prettify JS

--- js/main.js
+++ js/main.js
@@ -1,70 +1,69 @@
-
 function main() {
 
-(function () {
-   'use strict';
+    (function() {
+        'use strict';
 
-  	$('a.page-scroll').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-          if (target.length) {
-            $('html,body').animate({
-              scrollTop: target.offset().top - 40
-            }, 900);
-            return false;
-          }
-        }
-      });
-
-	// affix the navbar after scroll below header
-$('#nav').affix({
-      offset: {
-        top: $('header').height()
-      }
-});
-
-
-
-  	// Portfolio isotope filter
-    $(window).load(function() {
-        var $container = $('.portfolio-items');
-        $container.isotope({
-            filter: '*',
-            animationOptions: {
-                duration: 750,
-                easing: 'linear',
-                queue: false
+        $('a.page-scroll').click(function() {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top - 40
+                    }, 900);
+                    return false;
+                }
             }
         });
-        $('.cat a').click(function() {
-            $('.cat .active').removeClass('active');
-            $(this).addClass('active');
-            var selector = $(this).attr('data-filter');
+
+        // affix the navbar after scroll below header
+        $('#nav').affix({
+            offset: {
+                top: $('header').height()
+            }
+        });
+
+
+
+        // Portfolio isotope filter
+        $(window).load(function() {
+            var $container = $('.portfolio-items');
             $container.isotope({
-                filter: selector,
+                filter: '*',
                 animationOptions: {
                     duration: 750,
                     easing: 'linear',
                     queue: false
                 }
             });
-            return false;
+            $('.cat a').click(function() {
+                $('.cat .active').removeClass('active');
+                $(this).addClass('active');
+                var selector = $(this).attr('data-filter');
+                $container.isotope({
+                    filter: selector,
+                    animationOptions: {
+                        duration: 750,
+                        easing: 'linear',
+                        queue: false
+                    }
+                });
+                return false;
+            });
+
         });
 
-    });
 
+        console.log("Hello! Thank you for visting my website.");
+        console.log("I see that you are familiar with DevTools since you've opened up the console.")
+        console.log("A trick I really like is using the shorthand $$() to return all elements that match the CSS selector.")
 
-console.log("Hello! Thank you for visting my website.");
-console.log("I see that you are familiar with DevTools since you've opened up the console.")
-console.log("A trick I really like is using the shorthand $$() to return all elements that match the CSS selector.")
+        // Pretty Photo
+        $("a[rel^='prettyPhoto']").prettyPhoto({
+            social_tools: false
+        });
 
-  	// Pretty Photo
-	$("a[rel^='prettyPhoto']").prettyPhoto({
-		social_tools: false
-	});
-
-}());
+    }());
 
 
 }
